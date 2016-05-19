c55403fe7a9455db366d8258c15f6e634be55507
TwinTreesSoftware <TwinTreesSoftware@users.noreply.github.com>
2016-05-19 07:54:15 +0300

    Some updates

--- templates/js/Share.js
+++ templates/js/Share.js
@@ -1,6 +1,6 @@
-/* Этот простой скрипт позволяет поделиться вашей веб-страницей через социаьную сеть VKontakte */
+/* Fuck */
 
 var LOC = location.href;
 var URL = "http://vk.com/share.php?url="+LOC;
-document.write("<p><a href="+URL+" target='_blank'><img src='vk.png' alt='Поделиться!'></a></p>");
+document.write("<p><a href="+URL+" target='_blank'><img src='vk.png' alt='Share'></a></p>");
 
